20a50b55f1f0b135346c42d60fc7560e56f0d751
Chirag Soni <chiragsoni1214@gmail.com>
2026-09-09 05:52:48 +0530

    Remove all postgres references and enforce SQLite

--- backend/database.py
+++ backend/database.py
@@ -1,19 +1,29 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String, Float, Boolean, DateTime
+from database import Base
+import datetime
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./AEROCLICK_DB.db"
+class AirfareRecord(Base):
+    __tablename__ = "airfare_records"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    id = Column(Integer, primary_key=True, index=True)
+    origin = Column(String, index=True)
+    destination = Column(String, index=True)
+    airline = Column(String, index=True)
+    flight_number = Column(String)
+    base_fare = Column(Float)
+    taxes = Column(Float)
+    convenience_fee = Column(Float)
+    total_fare = Column(Float)
+    seat_availability_pct = Column(Float)
+    fare_class = Column(String)
+    source_portal = Column(String)
+    is_outlier = Column(Boolean, default=False)
+    timestamp = Column(DateTime, default=datetime.datetime.utcnow)
 
-Base = declarative_base()
+class User(Base):
+    __tablename__ = "users"
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    role = Column(String)
